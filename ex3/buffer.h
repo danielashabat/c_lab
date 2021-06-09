b826bb7ff1c3a2044330021fe63d1f75dcdf9740
Yair Mager <yairmager@gmail.com>
2021-06-09 16:05:34 +0300

    buffer module in LB

---
+++ ex3/buffer.h
@@ -0,0 +1,16 @@
+
+#ifndef BUFFER
+#define BUFFER
+typedef struct Buffer
+{
+    char* data;
+    int chars_allocated;
+    int chars_count;
+    int suffixes;
+} Buffer;
+
+void initialize_buffer(Buffer* buff);
+void free_buffer(Buffer* buff);
+void append_to_buffer(Buffer* buff, char* text, int chars_to_write);
+void print_buffer(Buffer* buff);
+#endif
