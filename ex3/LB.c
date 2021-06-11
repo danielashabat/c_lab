af8732781a9c2885a1ad6b440c2909d12ef9060e
Yair Mager <yairmager@gmail.com>
2021-06-11 17:44:50 +0300

    nothing

--- ex3/LB.c
+++ ex3/LB.c
@@ -6,23 +6,33 @@
 
 int main(int argc, char** argv)
 {
-  Buffer buff_ob;
-  Buffer* buff = &buff_ob;
-  buff->data = NULL;
-  initialize_buffer(buff);
+    Buffer buff_ob;
+    Buffer* buff = &buff_ob;
+    buff->data = NULL;
+    initialize_buffer(buff);
 
-  char* str1 = "na \r\n\r\n kk";
-  char* str2 = "olola\r\n\r\n";
+    char* str1 = "na \r\n\r\n kk";
+    char* str2 = "olola\r\n\r\n";
 
-  append_to_buffer(buff, str1, strlen(str1));
-  print_buffer(buff);
+    char* str3 = "AAA";
+    char* str4 = "BBB\r\n\r\n";
 
-  append_to_buffer(buff, str2, strlen(str2));
-  print_buffer(buff);
+    append_to_buffer(buff, str1, strlen(str1));
+    print_buffer(buff);
 
-  printf("%d\n", buff->suffixes);
+    append_to_buffer(buff, str2, strlen(str2));
+    print_buffer(buff);
 
-  free_buffer(buff);
+    initialize_buffer(buff);
+
+    append_to_buffer(buff, str3, strlen(str1));
+    print_buffer(buff);
+
+    append_to_buffer(buff, str4, strlen(str2));
+    print_buffer(buff);
+
+    printf("%d\n", buff->suffixes);
+    free_buffer(buff);
 }
 
 //    winSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
